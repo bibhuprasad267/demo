7ae2878a5d04f4cad66ca0206b0aa58938d02120
bibhuprasad267 <67335584+bibhuprasad267@users.noreply.github.com>
2020-06-24 08:50:04 +0530

    Demo website
    Demo website

---
+++ JS/master.js
@@ -0,0 +1,6 @@
+
+// function swtich_page(page_name) {
+//    let dispaly_page=document.getElementById(page_name);
+//    dispaly_page.style.display="block";
+//    dispaly_page.style.transitionDuration="0.5s"
+// }
